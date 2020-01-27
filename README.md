cf192b5ced7eb71d7535d363a6d13d4848eaa599
VeraV3 <veravukosavljevic0@gmail.com>
2020-01-27 21:48:53 +0100

    komentari

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RG181-ambar
 Ambar
+prevodjenje:
+
+gcc ambar.c image.c  -lglut -lGLU -lGL -lm
+
+pokretanje ./a.out
+
+Konj krece pritiskom na 'g', moze da se ubrza ili uspori pritiskom na 'f' ili 'd', na 's' se pauzira, na 'r' restartuje.
+Cilj je zaustaviti ga da ne ode van vidnog polja cuvarima sa 4 strane koji se krecu pritiscima na 2, 4, 6, 8 za levo, desno, gore, dole
+sve dok ne udje u ambar. Po polju trci i pas koji ga plasi. 
--- ambar.c
+++ ambar.c
@@ -395,18 +395,20 @@ static void on_display(){
     /* Iskljucujemo aktivnu teksturu */
     glBindTexture(GL_TEXTURE_2D, 0);
 
-  
+    //saljemo novu sliku na ekran  
   glutSwapBuffers();
 }
 
 
 static void on_keyboard(unsigned char key, int x, int y){
   switch(key){
+    //oslobanjamo memoriju i prekidamo program
   case 27:
     glDeleteTextures(10, names);
     exit(0);
     break;
   case 'r':
+    //restartujemo igru i sve parametre postavljamo na pocetne
     animation_ongoing = 0;
     srand(time(NULL));
     k_x = -(0.5-k_size/2) + (1-k_size)*rand()/(float) RAND_MAX;
@@ -426,25 +428,33 @@ static void on_keyboard(unsigned char key, int x, int y){
     break;
   case 'g':
   case 'G':
+    //pokrecemo animaciju
     if(!animation_ongoing){
       animation_ongoing = 1;
       glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
     }
     break;
+
+    //zaustavljamo animaciju
   case 's':
   case 'S':
     animation_ongoing = 0;
     break;
+
+    //uvecavamo brzinu dva puta
   case 'f':
   case 'F':
     k_vx*=2;
     k_vy*=2;
     break;
+
+    //usporavamo duplo
   case 'd':
   case 'D':
     k_vx/=2;
     k_vy/=2;
     break;
+    //pomeramo levo gornjeg i donjeg cuvara ako imamo gde
   case '4':
     if(lgornji_x > -1){
       lgornji_x-=l_size;
@@ -452,6 +462,7 @@ static void on_keyboard(unsigned char key, int x, int y){
       glutPostRedisplay();
     }
     break;
+    //pomeramo desno gornjeg i donjeg cuvara ako ima mesta desno
   case '6':
     if(lgornji_x <1){
       lgornji_x+=l_size;
@@ -459,6 +470,7 @@ static void on_keyboard(unsigned char key, int x, int y){
       glutPostRedisplay();
     }
     break;
+    //levi i desni cuvar idu gore ako vec nisu na vrhu
   case '8':
     if(llevi_y<1){
       llevi_y+=l_size;
@@ -466,6 +478,8 @@ static void on_keyboard(unsigned char key, int x, int y){
       glutPostRedisplay();
     }
     break;
+
+    //levi i desni cuvar idu dole ako nisu na dnu
   case '2':
     if(llevi_y>-1){
       llevi_y-=l_size;
@@ -478,19 +492,21 @@ static void on_keyboard(unsigned char key, int x, int y){
 }
 
 static void on_timer(int id){
+  //proveravamo da li je pravi tajmer
   if(id!=TIMER_ID)
     return;
-
+  //pomeramo psa u smeru u kom ide dok ne dodje do ivice, a onda mu menjamo smer
   p_x +=p_vx;
   if(p_x<=-(1-p_size/2) || p_x>=1-p_size/2)
     p_vx*=-1;
   p_y +=p_vy;
   if(p_y<=-(1-p_size/2) || p_y>=1-p_size/2)
     p_vy*=-1;
-
+  //pomeramo konja u smeru u kom ide
   k_x += k_vx;
   k_y += k_vy;
-  
+  //ako je rastojanje konja od pasa sa strane ili onog u sredini manje od zadatog
+  //menjamo smer kretanja
   if(sqrt((k_x - llevi_x)*(k_x-llevi_x)+(k_y-llevi_y)*(k_y-llevi_y)) <rastojanje
    || sqrt((k_x - ldesni_x)*(k_x-ldesni_x)+(k_y-ldesni_y)*(k_y-ldesni_y)) <rastojanje)
   
@@ -506,14 +522,14 @@ static void on_timer(int id){
        k_vy *= -1;
     }
 
-    
+    //ako je konj usao u ambar, smiruje se
     if(sqrt((k_x)*(k_x)+(k_y)*(k_y)) <velicina_ambara/2){
       k_x = 0;
       k_y = 0;
     }    
-    
+    //reiscrtavamo  
   glutPostRedisplay();
-  
+  //pozivamo sledeci tajmer
   if(animation_ongoing){
     glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
   }
